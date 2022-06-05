b3c394fef9eea07c76a719e20f9491159b7f48da
Dave <87390532+vadedes@users.noreply.github.com>
2022-06-05 19:40:39 +0800

    completed header and search bar

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,41 @@
 import './header.css';
 
 const Header = () => {
-    return <div>Header</div>;
+    return (
+        <div className='header'>
+            <div className='header-container'>
+                <div className='sub-nav-container'>
+                    <div className='sub-nav-item active'>
+                        <span>Stays</span>
+                    </div>
+                    <div className='sub-nav-item'>
+                        <span>Flights</span>
+                    </div>
+                    <div className='sub-nav-item'>
+                        <span>Car rentals</span>
+                    </div>
+                    <div className='sub-nav-item'>
+                        <span>Attractions</span>
+                    </div>
+                    <div className='sub-nav-item'>
+                        <span>Airport taxis</span>
+                    </div>
+                </div>
+                <div className='header-content-wrapper'>
+                    <h1>A lifetime of discounts? It's Genius.</h1>
+                    <p>Get rewarded for your travels - unlock instant savings of 10% or more with a free Trip Hub account</p>
+                    <button>Sign in/Register</button>
+                </div>
+                <div className='search-bar-container'>
+                    <form className='search-input'>
+                        <input className='search-form-control' type='text' placeholder='Where are you going?' />
+                    </form>
+                    <div>Calendar</div>
+                    <div>Options</div>
+                    <div>Search Button</div>
+                </div>
+            </div>
+        </div>
+    );
 };
 export default Header;
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,6 +1,18 @@
 import './navbar.css';
 
 const Navbar = () => {
-    return <div>Navbar</div>;
+    return (
+        <div className='navbar'>
+            <div className='nav-container'>
+                <div>
+                    <span className='logo'>TripHub</span>
+                </div>
+                <div className='btn-container'>
+                    <button>Register</button>
+                    <button>Login</button>
+                </div>
+            </div>
+        </div>
+    );
 };
 export default Navbar;
